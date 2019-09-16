a423ff2beb505c7e672cfef87b9dcdf94085ef97
Aleksandr Kravtsov <sabahtalateh@gmail.com>
2019-09-16 13:14:16 +0300

    WIP

---
+++ dev/blockchain.js
@@ -0,0 +1,26 @@
+export class Blockchain {
+    constructor() {
+        this.chain = [];
+        this.newTransactions = [];
+    }
+
+    createNewBlock(nonce, prevBlockHash, hash) {
+        const newBlock = {
+            index: this.chain.length + 1,
+            timestamp: Date.now(),
+            transactions: this.newTransactions,
+            nonce,
+            hash,
+            prevBlockHash,
+        };
+
+        this.newTransactions = [];
+        this.chain.push(newBlock);
+
+        return newBlock;
+    }
+
+    getLastBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+}
---
+++ dev/test.js
@@ -0,0 +1,10 @@
+import {Blockchain} from './blockchain';
+
+const blockchain = new Blockchain();
+
+blockchain.createNewBlock(8821, 'LKJALKSJD1283KJLJ123', 'NUOWNQ1LAKJ1239123LJ');
+blockchain.createNewBlock(182, 'KJHASD9123', 'JASD1923KKJ');
+blockchain.createNewBlock(8293, 'JA9LKAJS123', 'KJA1823JLAKJ');
+
+
+console.log(blockchain);
